0b3137e622e8d6dc39bab912a3bd59fe55dace73
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-07-09 16:44:44 +0700

    Splashscreen

--- app/src/main/java/com/nandaiqbalh/aksaralibrary/onboarding/SplashScreenActivity.kt
+++ app/src/main/java/com/nandaiqbalh/aksaralibrary/onboarding/SplashScreenActivity.kt
@@ -1,12 +1,31 @@
 package com.nandaiqbalh.aksaralibrary.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import com.nandaiqbalh.aksaralibrary.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // full screen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // create variable for splashscreen handler
+        val handler = Handler()
+        handler.postDelayed({
+
+            val intent = Intent(this@SplashScreenActivity, OnboardingOneActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 5000)
     }
 }
