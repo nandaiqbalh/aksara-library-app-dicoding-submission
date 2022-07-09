2be949bcbf407d82ca8c03f1c8dc2d4132848c7e
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-07-09 19:07:57 +0700

    Profile

--- app/src/main/java/com/nandaiqbalh/aksaralibrary/home/HomeActivity.kt
+++ app/src/main/java/com/nandaiqbalh/aksaralibrary/home/HomeActivity.kt
@@ -1,12 +1,41 @@
 package com.nandaiqbalh.aksaralibrary.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.nandaiqbalh.aksaralibrary.R
+import com.nandaiqbalh.aksaralibrary.profile.ProfileActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var btnProfile: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // init
+        init()
+
+        // mainButton
+        mainButton()
+    }
+
+    private fun init(){
+
+        btnProfile = findViewById(R.id.btn_profile)
+
+    }
+
+    private fun mainButton(){
+
+        btnProfile.setOnClickListener {
+
+            val intent = Intent(this@HomeActivity, ProfileActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
--- app/src/main/java/com/nandaiqbalh/aksaralibrary/profile/ProfileActivity.kt
+++ app/src/main/java/com/nandaiqbalh/aksaralibrary/profile/ProfileActivity.kt
@@ -1,12 +1,42 @@
 package com.nandaiqbalh.aksaralibrary.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.nandaiqbalh.aksaralibrary.R
+import com.nandaiqbalh.aksaralibrary.home.HomeActivity
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var btnBack: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        // init
+        init()
+
+        // main button
+        mainButton()
+
+    }
+
+    private fun init(){
+
+        btnBack = findViewById(R.id.btn_back)
+
+    }
+
+    private fun mainButton(){
+
+        btnBack.setOnClickListener {
+
+            val intent = Intent(this@ProfileActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finishAffinity()
+
+        }
     }
 }
