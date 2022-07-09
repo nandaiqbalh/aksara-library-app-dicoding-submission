887d0bfe3a99ac39ecc0105d8a610c85755b6886
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-07-09 16:22:04 +0700

    FE: Detail Book

---
+++ app/src/main/java/com/nandaiqbalh/aksaralibrary/detail/DetailBookActivity.kt
@@ -0,0 +1,12 @@
+package com.nandaiqbalh.aksaralibrary.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nandaiqbalh.aksaralibrary.R
+
+class DetailBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_book)
+    }
+}
